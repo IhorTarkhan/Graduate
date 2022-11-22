31d482eeb44bfe447ea86358b3267d611e12fc06
IhorTarkhan <ihor.tarkhan.working@gmail.com>
2022-11-22 14:10:52 +0200

    scrap refactoring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src.scraping import scrap
 
 if __name__ == '__main__':
-    scrap("abc", "en-US", "/Users/ihor/Downloads")
+    scrap("abc", "en-US")
--- src/scraping.py
+++ src/scraping.py
@@ -2,33 +2,28 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import Select
 
 
-def scrap(text: str, language: str, download_directory: str = None):
-    if download_directory is None:
-        site = webdriver.Chrome()
-    else:
-        options = webdriver.ChromeOptions()
-        prefs = {"download.default_directory": download_directory}
-        options.add_experimental_option("prefs", prefs)
-        site = webdriver.Chrome(options=options)
+def scrap(text: str, language: str):
+    driver = webdriver.Chrome()
+    driver.get("https://soundoftext.com/")
 
-    site.get("https://soundoftext.com/")
+    def scroll_to(element: WebElement):
+        driver.execute_script("arguments[0].scrollIntoView();", element)
 
-    text_area = site.find_element(By.CLASS_NAME, "field__textarea")
+    text_area = driver.find_element(By.CLASS_NAME, "field__textarea")
+    language_select = Select(driver.find_element(By.CLASS_NAME, "field__select"))
+    submit_button = driver.find_element(By.CLASS_NAME, "field__submit")
+
     text_area.send_keys(text)
+    language_select.select_by_value(language)
 
-    select = Select(site.find_element(By.CLASS_NAME, "field__select"))
-    select.select_by_value(language)
-
-    button = site.find_element(By.CLASS_NAME, "field__submit")
-    button.click()
+    submit_button.click()
     time.sleep(5)
 
-    play_actions = site.find_element(By.XPATH, "//a[@class='card__action' and text()='Download']")
-    site.execute_script("arguments[0].scrollIntoView();", play_actions)
-    play_actions.click()
+    download_button = driver.find_element(By.XPATH, "//a[@class='card__action' and text()='Download']")
+    scroll_to(download_button)
+    download_button.click()
     time.sleep(3)
-
-    print(1)
