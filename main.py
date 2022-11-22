d9471914625d6cbf4b3f13232136a143d6efde62
IhorTarkhan <ihor.tarkhan.working@gmail.com>
2022-11-22 16:53:05 +0200

    extract main method

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from src.fetch import fetch
 from src.scraping import scrap
 
-if __name__ == '__main__':
+
+def main():
     fetch({"en-US": ["abc", "apple"], "uk-UA": ["кіт", "собака"]})
     scrap({"en-US": ["abc", "apple"], "uk-UA": ["кіт", "собака"]})
+
+
+if __name__ == '__main__':
+    main()
