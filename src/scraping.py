904b8fbd84a2cb98421ce5e06fead97a9d8d7ba3
IhorTarkhan <ihor.tarkhan.working@gmail.com>
2022-11-23 19:13:46 +0200

    scratch rework saving

--- src/scraping.py
+++ src/scraping.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
@@ -6,26 +9,33 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def scrap(data: dict[str, list[str]]):
-    driver = webdriver.Chrome()
-    driver.get("https://soundoftext.com/")
+def _get_download_folder(language: str) -> str:
+    return os.path.join(os.getcwd(), "audio_files", language)
 
-    def scroll_to(element: WebElement) -> WebElement:
-        driver.execute_script("arguments[0].scrollIntoView();", element)
-        return element
 
-    text_area = driver.find_element(By.CLASS_NAME, "field__textarea")
-    language_select = driver.find_element(By.CLASS_NAME, "field__select")
-    submit_button = driver.find_element(By.CLASS_NAME, "field__submit")
-
+def scrap(data: dict[str, list[str]]) -> None:
     for language, texts in data.items():
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option("prefs", {"download.default_directory": _get_download_folder(language)})
+        driver = webdriver.Chrome(options=options)
+        driver.get("https://soundoftext.com/")
+
+        def scroll_to(element: WebElement) -> WebElement:
+            driver.execute_script("arguments[0].scrollIntoView();", element)
+            return element
+
+        text_area = driver.find_element(By.CLASS_NAME, "field__textarea")
+        language_select = driver.find_element(By.CLASS_NAME, "field__select")
+        submit_button = driver.find_element(By.CLASS_NAME, "field__submit")
+
         Select(scroll_to(language_select)).select_by_value(language)
         for text in texts:
             scroll_to(text_area).send_keys(text)
             scroll_to(submit_button).click()
 
-    WebDriverWait(driver, 60).until_not(ec.presence_of_element_located((By.CLASS_NAME, 'sk-spinner')))
-    download_buttons = driver.find_elements(By.XPATH, "//a[@class='card__action' and text()='Download']")
-    for download_button in download_buttons:
-        scroll_to(download_button).click()
-    input()
+        WebDriverWait(driver, 60).until_not(ec.presence_of_element_located((By.CLASS_NAME, "sk-spinner")))
+        download_buttons = driver.find_elements(By.XPATH, "//a[@class='card__action' and text()='Download']")
+        for download_button in download_buttons:
+            scroll_to(download_button).click()
+        time.sleep(5)
+        driver.quit()
