9c85021e860fb739f0e4d00e06356a6834030529
IhorTarkhan <ihor.tarkhan.working@gmail.com>
2022-11-24 19:42:25 +0200

    rename private method

--- src/db/chat_db.py
+++ src/db/chat_db.py
@@ -15,6 +15,10 @@ def __create_table_if_not_exists(cursor: Cursor) -> None:
         """)
 
 
+def __create_if_not_exist(tg_id: int) -> None:
+    sql_util.not_select("INSERT OR IGNORE INTO chat(tg_id) VALUES (?);", [tg_id], __create_table_if_not_exists)
+
+
 def upsert(tg_id: int) -> None:
     sql_util.not_select(
         """
@@ -25,16 +29,12 @@ def upsert(tg_id: int) -> None:
         """, [tg_id, tg_id], __create_table_if_not_exists)
 
 
-def create_if_not_exist(tg_id: int) -> None:
-    sql_util.not_select("INSERT OR IGNORE INTO chat(tg_id) VALUES (?);", [tg_id], __create_table_if_not_exists)
-
-
 def update_last_action(tg_id: int, last_action: str) -> None:
     sql_util.not_select("UPDATE chat SET last_action = ? WHERE tg_id = ?", [last_action, tg_id],
-                        lambda c: create_if_not_exist(tg_id))
+                        lambda c: __create_if_not_exist(tg_id))
 
 
 def find_by_id(tg_id: int) -> Chat:
     select = sql_util.select_one("SELECT tg_id, last_action FROM chat WHERE tg_id = ?", [tg_id],
-                                 lambda c: create_if_not_exist(tg_id))
+                                 lambda c: __create_if_not_exist(tg_id))
     return Chat(select[0], select[1])
