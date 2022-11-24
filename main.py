e64965a5c0bee3b6f1c79aea42d9738f67fa3991
IhorTarkhan <ihor.tarkhan.working@gmail.com>
2022-11-24 16:18:49 +0200

    rework scraping

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from src.scraping import scrap
 
 
 def main():
-    fetch({"en-US": ["abc", "apple"], "uk-UA": ["кіт", "собака"]})
     scrap({"en-US": ["abc", "apple"], "uk-UA": ["кіт", "собака"]})
+    fetch({"en-US": ["abc", "apple"], "uk-UA": ["кіт", "собака"]})
 
 
 if __name__ == "__main__":
