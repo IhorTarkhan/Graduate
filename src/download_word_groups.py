da3e71ed230299c3424517917350803c49b75ac3
IhorTarkhan <ihor.tarkhan.working@gmail.com>
2022-11-27 22:44:59 +0200

    rearrange code

--- src/download_word_groups.py
+++ src/download_word_groups.py
@@ -6,21 +6,17 @@ from selenium.webdriver.remote.webelement import WebElement
 
 def download_word_groups():
     driver: WebDriver = webdriver.Chrome()
-    driver.get("https://learnenglish.britishcouncil.org/vocabulary/a1-a2-vocabulary")
 
-    images: list[WebElement] = driver.find_elements(By.CLASS_NAME, "field--name-field-image")
-    images.pop()
-    images.pop(0)
-    links = list(map(lambda x: x.find_element(By.TAG_NAME, "a").get_attribute("href"), images))
-    for link in links:
-        print(link)
+    def scrap_word_groups(link: str):
+        driver.get(link)
 
+        images: list[WebElement] = driver.find_elements(By.CLASS_NAME, "field--name-field-image")
+        images.pop()
+        images.pop(0)
+        links = list(map(lambda x: x.find_element(By.TAG_NAME, "a").get_attribute("href"), images))
+        for link in links:
+            print(link)
+
+    scrap_word_groups("https://learnenglish.britishcouncil.org/vocabulary/a1-a2-vocabulary")
     print("---------------")
-    driver.get("https://learnenglish.britishcouncil.org/vocabulary/b1-b2-vocabulary")
-
-    images: list[WebElement] = driver.find_elements(By.CLASS_NAME, "field--name-field-image")
-    images.pop()
-    images.pop(0)
-    links = list(map(lambda x: x.find_element(By.TAG_NAME, "a").get_attribute("href"), images))
-    for link in links:
-        print(link)
+    scrap_word_groups("https://learnenglish.britishcouncil.org/vocabulary/b1-b2-vocabulary")
