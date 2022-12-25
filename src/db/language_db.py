7a7fd03101d35961a9a02ef37da2c6f9f8739b39
IhorTarkhan <ihor.tarkhan.working@gmail.com>
2022-12-25 10:55:31 +0200

    languages add translation api code

--- src/db/language_db.py
+++ src/db/language_db.py
@@ -1,3 +1,5 @@
+from lazy_streams import stream
+
 from src.db.Transaction import Transaction
 
 
@@ -6,31 +8,32 @@ class Language:
         """
             CREATE TABLE IF NOT EXISTS language
             (
-                code TEXT PRIMARY KEY,
-                name TEXT
+                code               TEXT PRIMARY KEY,
+                name               TEXT,
+                translate_api_code TEXT
             );
         """
     ]
 
-    def __init__(self, code: str, name: str):
+    def __init__(self, code: str, name: str, translate_api_code: str):
         self.code: str = code
         self.name: str = name
+        self.translate_api_code: str = translate_api_code
 
 
-def insert(values: list[tuple[str, str]]):
-    sql = "INSERT INTO language(code, name) VALUES {0};".format(", ".join(map(lambda x: "(?, ?)", values)))
-    parameters = []
-    for code, name in values:
-        parameters.append(code)
-        parameters.append(name)
+def insert(values: list[Language]):
+    sql = "INSERT INTO language(code, name, translate_api_code) VALUES {0};" \
+        .format(", ".join(map(lambda x: "(?, ?, ?)", values)))
+    parameters = stream(values) \
+        .map(lambda v: [v.code, v.name, v.translate_api_code]) \
+        .flatten() \
+        .to_list()
     Transaction.change(sql, parameters)
 
 
 def find_all() -> list[Language]:
-    return list(map(
-        lambda e: Language(e[0], e[1]),
-        Transaction.select("SELECT code, name FROM language;")
-    ))
+    select = Transaction.select("SELECT code, name, translate_api_code FROM language;")
+    return stream(select).map(lambda e: Language(e[0], e[1], e[2])).to_list()
 
 
 def find_count() -> int:
@@ -38,5 +41,5 @@ def find_count() -> int:
 
 
 def find_by_name(name: str) -> Language:
-    select = Transaction.select_one("SELECT code, name FROM language WHERE name = ?;", [name])
-    return Language(select[0], select[1])
+    select = Transaction.select_one("SELECT code, name, translate_api_code FROM language WHERE name = ?;", [name])
+    return Language(select[0], select[1], select[2])
--- src/service/basic_languages.py
+++ src/service/basic_languages.py
@@ -1,22 +1,23 @@
 import logging
 
 from src.db import language_db
+from src.db.language_db import Language
 
 
 def insert_basic_languages():
     if language_db.find_count() == 0:
         logging.warning("Languages list is empty, inserting")
         language_db.insert([
-            ("en-US", "English"),
-            ("cmn-Hant-TW", "Chinese"),
-            ("es-ES", "Spanish"),
-            ("fr-FR", "French"),
-            ("pt-BR", "Portuguese"),
-            ("uk-UA", "Ukrainian"),
-            ("de-DE", "German"),
-            ("it-IT", "Italian"),
-            ("ja-JP", "Japan"),
-            ("pl-PL", "Polish")
+            Language("en-US", "English", "en"),
+            Language("cmn-Hant-TW", "Chinese", "zh"),
+            Language("es-ES", "Spanish", "es"),
+            Language("fr-FR", "French", "fr"),
+            Language("pt-BR", "Portuguese", "pt"),
+            Language("uk-UA", "Ukrainian", "uk"),
+            Language("de-DE", "German", "de"),
+            Language("it-IT", "Italian", "it"),
+            Language("ja-JP", "Japanese", "ja"),
+            Language("pl-PL", "Polish", "pl")
         ])
     else:
         logging.info(f"Skip download languages, already {language_db.find_count()} in db")
